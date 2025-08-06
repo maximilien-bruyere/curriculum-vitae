83e7563c4fc76e6c43638de10dc9eb32ebc780d8
Bruyère Maximilien <maximilien-bruyere@hotmail.com>
2025-08-07 01:36:40 +0200

    feat: add header with navbar (not works correctly) and form to change language (works)

--- src/pages/header/Header.tsx
+++ src/pages/header/Header.tsx
@@ -1,19 +1,9 @@
-import { useTranslation } from "react-i18next";
-import Button from "./button/Button";
+import Nav from "./nav/Nav";
 
 const Header = () => {
-  const { t } = useTranslation();
   return (
-    <header className="bg-background text-text p-10 shadow mb-8">
-      <div className="max-w-3xl mx-auto text-center">
-        <h1 className="text-3xl font-bold mb-2">{t("name")}</h1>
-        <h2 className="text-accent text-xl mb-4">{t("student")}</h2>
-        <div className="flex flex-col sm:flex-row justify-center items-center gap-4 text-text text-sm">
-          <span>📧 {t("email")}</span>
-          <span>🌍 {t("place")}</span>
-        </div>
-        <Button>{t("contact")}</Button>
-      </div>
+    <header className="flex flex-col items-center justify-center w-full ">
+      <Nav />
     </header>
   );
 };
---
+++ src/pages/header/nav/Nav.tsx
@@ -0,0 +1,77 @@
+import { useState } from "react";
+import { useTranslation } from "react-i18next";
+
+const Nav = () => {
+  const { t, i18n } = useTranslation();
+  const [active, setActive] = useState("home");
+  const [lang, setLang] = useState(i18n.language.toUpperCase().slice(0,2) || "FR");
+
+  const handleLangChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const newLang = e.target.value;
+    setLang(newLang);
+    i18n.changeLanguage(newLang.toLowerCase());
+    e.target.blur();
+  };
+
+  return (
+    <nav className="fixed top-0 right-0 z-20 p-4 w-full flex justify-around items-center bg-white shadow-sm">
+      <ul className="flex space-x-4">
+        <li>
+          <a
+            href="#home"
+            onClick={() => setActive("home")}
+            className={`text-[15px] transition-all duration-200 text-gray-700 underline-offset-10 active:text-rust ${
+              active === "home"
+                ? "underline decoration-rust decoration-2 text-rust"
+                : ""
+            }`}
+          >
+            {t("home")}
+          </a>
+        </li>
+        <li>
+          <a
+            href="#portfolio"
+            onClick={() => setActive("portfolio")}
+            className={`text-[15px] transition-all duration-200 text-gray-700 underline-offset-10 active:text-rust ${
+              active === "portfolio"
+                ? "underline decoration-rust decoration-2 text-rust"
+                : ""
+            }`}
+          >
+            {t("portfolio")}
+          </a>
+        </li>
+         <li>
+          <a
+            href="#cv"
+            onClick={() => setActive("cv")}
+            className={`text-[15px] transition-all duration-200 text-gray-700 underline-offset-10 active:text-rust ${
+              active === "cv"
+                ? "underline decoration-rust decoration-2 text-rust"
+                : ""
+            }`}
+          >
+            {t("cv")}
+          </a>
+        </li>
+      </ul>
+      <form className="">
+        <label htmlFor="under line_select" className="sr-only">
+          Underline select
+        </label>
+        <select
+          id="underline_select"
+          value={lang}
+          onChange={handleLangChange}
+          className="hover:cursor-pointer text-[15px] block pt-2 pb-1 px-2 w-full text-gray-700 bg-transparent border-0 border-b-1 border-rust appearance-none focus:outline-none focus:ring-0 focus:border-gray-200 peer"
+        >
+          <option value="FR">{t("french")}</option>
+          <option value="EN">{t("english")}</option>
+        </select>
+      </form>
+    </nav>
+  );
+};
+
+export default Nav;
