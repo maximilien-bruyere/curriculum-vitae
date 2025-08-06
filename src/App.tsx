304a62c38f9852b36d9baddbd6cb12a02cd62ee1
Bruyère Maximilien <maximilien-bruyere@hotmail.com>
2025-08-07 01:40:44 +0200

    feat: add header, main and footer section

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
-import Footer from "./pages/footer/Footer";
+import Background from "./pages/background/Background";
 import Header from "./pages/header/Header";
-import Home from "./pages/home/Home";
+import Main from "./pages/main/Main";
+import Footer from "./pages/footer/Footer";
+import "./App.css";
 
 function App() {
   return (
-    <>
-      <Header />
-      <Home />
-      <Footer />
-    </>
+    <div className="relative h-screen">
+      <Background />
+      <div className="relative flex flex-col pt-80 px-10">
+        <div>
+          <Header />
+          <Main />
+          <Footer />
+        </div>
+      </div>
+    </div>
   );
 }
 
