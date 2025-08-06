7d31c706f25aec2f9db4af5f1931227fe8c6770b
Bruyère Maximilien <maximilien-bruyere@hotmail.com>
2025-08-07 01:38:57 +0200

    style: add background

---
+++ src/pages/background/Background.tsx
@@ -0,0 +1,11 @@
+const Background = () => {
+  return (
+    <div className="absolute inset-0">
+      <div className="relative h-full w-full bg-red [&>div]:absolute [&>div]:h-full [&>div]:w-full [&>div]:bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [&>div]:[background-size:16px_16px] [&>div]:[mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_70%,transparent_100%)]">
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Background;
